90d2e49f312bf7227d0f1aea87f926085fcf915f
Liam Cates <LiamJCates@gmail.com>
2020-09-02 22:20:18 -0700

    Resume structure and style

---
+++ javascript/resume.js
@@ -0,0 +1,49 @@
+function openTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tab-border", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " tab-border";
+}
+
+function openBottomTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("bottomTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("bottomTablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tab-border", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " tab-border";
+}
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+/* TODO ternary */
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
